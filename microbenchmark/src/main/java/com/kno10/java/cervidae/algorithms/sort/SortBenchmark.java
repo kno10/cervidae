e9a12b28209f351a3216cfa8370d532a9edd7941
Erich Schubert <kno10@github.fake>
2013-04-02 01:23:29 +0200

    Include Java sort in the benchmark.

--- microbenchmark/src/main/java/com/kno10/java/cervidae/algorithms/sort/SortBenchmark.java
+++ microbenchmark/src/main/java/com/kno10/java/cervidae/algorithms/sort/SortBenchmark.java
@@ -1,14 +1,22 @@
 package com.kno10.java.cervidae.algorithms.sort;
 
+import java.util.Arrays;
 import java.util.Random;
 
 import com.google.caliper.Benchmark;
 import com.google.caliper.Param;
 import com.google.caliper.runner.CaliperMain;
+import com.kno10.java.cervidae.adapter.arraylike.ArraySortAdapter;
+import com.kno10.java.cervidae.adapter.arraylike.ComparableArrayAdapter;
 import com.kno10.java.cervidae.adapter.arraylike.DoubleArrayAdapter;
 
+/**
+ * Benchmark class for sorting algorithms.
+ * 
+ * @author Erich Schubert
+ */
 public class SortBenchmark extends Benchmark {
-  @Param({ "1000" })
+  @Param({ "10", "100", "1000", "10000" })
   int size;
 
   static final String BASE = "com.kno10.java.cervidae.algorithms.sort.";
@@ -17,7 +25,9 @@ public class SortBenchmark extends Benchmark {
   BASE + "DualPivotQuickSortBo5", //
   BASE + "QuickSortBo5", //
   BASE + "QuickSortBo3", //
-  BASE + "QuickSortTextbook"//
+  BASE + "QuickSortTextbook",//
+  // BASE + "InsertionSort",//
+  BASE + "SortBenchmark$JavaSort", //
   })
   String algname;
 
@@ -46,7 +56,7 @@ public class SortBenchmark extends Benchmark {
     }
   }
 
-  public double timeSort(int reps) {
+  public double timeSortPrimitiveDoubles(int reps) {
     double ret = 0.0;
     for(int i = 0; i < reps; i++) {
       double[] tmp = array.clone();
@@ -54,6 +64,34 @@ public class SortBenchmark extends Benchmark {
       ret += tmp[0] + tmp[tmp.length - 1];
     }
     return ret;
+  }
+
+  public double timeSortObjects(int reps) {
+    double ret = 0.0;
+    for(int i = 0; i < reps; i++) {
+      Double[] tmp = new Double[array.length];
+      for(int j = 0; j < array.length; j++) {
+        tmp[j] = array[j];
+      }
+      alg.sort(new ComparableArrayAdapter<>(), tmp);
+      ret += tmp[0] + tmp[tmp.length - 1];
+    }
+    return ret;
+  }
+
+  class JavaSort extends AbstractArraySortAlgorithm {
+    @Override
+    public <T> void sort(ArraySortAdapter<? super T> adapter, T data, int start, int end) {
+      if(adapter instanceof DoubleArrayAdapter) {
+        Arrays.sort((double[]) data, start, end);
+      }
+      else if(adapter instanceof ComparableArrayAdapter) {
+        Arrays.sort((Object[]) data, start, end);
+      }
+      else {
+        throw new RuntimeException("Can't sort with official Java classes.");
+      }
+    }
   }
 
   public static void main(String[] args) {
