efaab4a358c5c8b74273ec303d7e6eae5602532e
Erich Schubert <kno10@github.fake>
2013-04-02 18:37:08 +0200

    Add optimized QuickSort for primitive types.

---
+++ algorithms/src/main/templates/java/com/kno10/java/cervidae/adapter/comparator/PrimitiveComparator.java
@@ -0,0 +1,18 @@
+package com.kno10.java.cervidae.adapter.comparator;
+
+/**
+ * Interface for comparing two ${Type}.
+ * 
+ * @author Erich Schubert
+ */
+//${NOTE}
+public interface ${Type}Comparator {
+  /**
+   * Compare two ${Type}.
+   * 
+   * @param x First ${api-type}
+   * @param y Second ${api-type}
+   * @return Comparison result
+   */
+  int compare(${api-type} x, ${api-type} y);
+}
