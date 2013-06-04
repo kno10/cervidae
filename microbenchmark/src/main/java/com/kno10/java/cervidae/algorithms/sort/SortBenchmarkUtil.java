2731332be2276a75b52548efc270dffe49dc2446
Erich Schubert <kno10@github.fake>
2013-06-04 19:19:46 +0200

    Add new data generator: distances in a 10d gaussian cluster.

--- microbenchmark/src/main/java/com/kno10/java/cervidae/algorithms/sort/SortBenchmarkUtil.java
+++ microbenchmark/src/main/java/com/kno10/java/cervidae/algorithms/sort/SortBenchmarkUtil.java
@@ -40,14 +40,35 @@ public final class SortBenchmarkUtil {
         return 0;
       }
     },
-    ;
+    NORMAL10D {
+      @Override
+      public double val(int pos, int length) {
+        return 0;
+      }
+    };
     abstract public double val(int pos, int length);
   }
 
   public static double[] generateRandomDoubleData(int size, MacroPattern pattern, double randomness, long seed) {
     double[] array = new double[size];
     Random rnd = new Random(seed);
-    for(int i = 0; i < size; i++) {
+
+    if (MacroPattern.NORMAL10D.equals(pattern)) {
+      double[] ref = new double[10];
+      for (int d = 0; d < 10; d++) {
+        ref[d] = rnd.nextGaussian();
+      }
+      for (int i = 0; i < size; i++) {
+        double dist = 0.0;
+        for (int d = 0; d < 10; d++) {
+          double v = ref[d] - rnd.nextGaussian();
+          dist += v * v;
+        }
+        array[i] = dist;
+      }
+      return array;
+    }
+    for (int i = 0; i < size; i++) {
       array[i] = rnd.nextDouble() * randomness + (1. - randomness) * pattern.val(i, size);
     }
     return array;
@@ -56,7 +77,22 @@ public final class SortBenchmarkUtil {
   public static int[] generateRandomIntegerData(int size, MacroPattern pattern, double randomness, long seed) {
     int[] array = new int[size];
     Random rnd = new Random(seed);
-    for(int i = 0; i < size; i++) {
+    if (MacroPattern.NORMAL10D.equals(pattern)) {
+      double[] ref = new double[10];
+      for (int d = 0; d < 10; d++) {
+        ref[d] = rnd.nextGaussian();
+      }
+      for (int i = 0; i < size; i++) {
+        double dist = 0.0;
+        for (int d = 0; d < 10; d++) {
+          double v = ref[d] - rnd.nextGaussian();
+          dist += v * v;
+        }
+        array[i] = (int)(dist * 1000);
+      }
+      return array;
+    }
+    for (int i = 0; i < size; i++) {
       array[i] = (int) (Integer.MAX_VALUE * (rnd.nextDouble() * randomness + (1. - randomness) * pattern.val(i, size)));
     }
     return array;
@@ -68,10 +104,10 @@ public final class SortBenchmarkUtil {
   public static String[] generateRandomStringData(int size, MacroPattern pattern, double randomness, long seed) {
     String[] array = new String[size];
     Random rnd = new Random(seed);
-    for(int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++) {
       final int len = 5 + rnd.nextInt(11); // 5-15 chars.
       final char[] buf = new char[len];
-      for(int j = 0; j < len; j++) {
+      for (int j = 0; j < len; j++) {
         buf[j] = (char) (FIRST_CHAR + (LAST_CHAR - FIRST_CHAR) * (rnd.nextDouble() * randomness + (1. - randomness) * pattern.val(i, size)));
       }
       array[i] = new String(buf);
