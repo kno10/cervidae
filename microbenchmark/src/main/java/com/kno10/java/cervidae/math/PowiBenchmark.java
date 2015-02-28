c0915cad8992dd09a712deec59d8eb628d56003e
Erich Schubert <kno10@github.fake>
2015-02-28 19:05:13 +0100

    Even faster powi, but also slightly more complicated.

--- microbenchmark/src/main/java/com/kno10/java/cervidae/math/PowiBenchmark.java
+++ microbenchmark/src/main/java/com/kno10/java/cervidae/math/PowiBenchmark.java
@@ -49,12 +49,12 @@ public class PowiBenchmark {
    * @return {@code Math.pow(x, p)}
    */
   public static double powi(double x, int p) {
-    if (p < 0) {
+    if(p < 0) {
       return Math.pow(x, p);
     }
     double tmp = x, ret = 1.;
-    for (int i = p; i > 0; i >>= 1) {
-      if ((i & 1) != 0) {
+    for(int i = p; i > 0; i >>= 1) {
+      if((i & 1) != 0) {
         ret *= tmp;
       }
       tmp *= tmp;
@@ -70,12 +70,12 @@ public class PowiBenchmark {
    * @return {@code Math.pow(x, p)}
    */
   public static double powi2(double x, int p) {
-    if (p < 2) {
+    if(p < 2) {
       return (p == 1) ? x : (p == 2) ? x * x : Math.pow(x, p);
     }
     double tmp = x, ret = 1.;
-    for (int i = p; i > 0; i >>= 1) {
-      if ((i & 1) != 0) {
+    for(int i = p; i > 0; i >>= 1) {
+      if((i & 1) != 0) {
         ret *= tmp;
       }
       tmp *= tmp;
@@ -91,12 +91,12 @@ public class PowiBenchmark {
    * @return {@code Math.pow(x, p)}
    */
   public static double powi3(double x, int p) {
-    if (p < 2) {
+    if(p < 2) {
       return Math.pow(x, p);
     }
     double tmp = x, ret = 1.;
-    for (int i = p; i > 0; i >>= 1) {
-      if ((i & 1) != 0) {
+    for(int i = p; i > 0; i >>= 1) {
+      if((i & 1) != 0) {
         ret *= tmp;
       }
       tmp *= tmp;
@@ -112,16 +112,16 @@ public class PowiBenchmark {
    * @return {@code Math.pow(x, p)}
    */
   public static double powi4(double x, int p) {
-    if (p < 2) {
+    if(p < 2) {
       return Math.pow(x, p);
     }
     double tmp = x, ret = 1.;
     int i = p;
-    while (true) {
-      if (i == 1) {
+    while(true) {
+      if(i == 1) {
         return ret * tmp;
       }
-      if ((i & 1) != 0) {
+      if((i & 1) != 0) {
         ret *= tmp;
       }
       tmp *= tmp;
@@ -137,16 +137,44 @@ public class PowiBenchmark {
    * @return {@code Math.pow(x, p)}
    */
   public static double powi5(double x, int p) {
-    if (p <= 2) {
+    if(p <= 2) {
       return Math.pow(x, p);
     }
     double tmp = x, ret = (p & 1) == 1 ? x : 1.;
     p >>= 1;
-    while (true) {
-      if (p == 1) {
+    while(true) {
+      if(p == 1) {
         return ret * tmp;
       }
-      if ((p & 1) != 0) {
+      if((p & 1) != 0) {
+        ret *= tmp;
+      }
+      tmp *= tmp;
+      p >>= 1;
+    }
+  }
+
+  /**
+   * Fast loop for computing {@code Math.pow(x, p)} for p >= 0 integer.
+   * 
+   * @param x Base
+   * @param p Exponent
+   * @return {@code Math.pow(x, p)}
+   */
+  public static double powi6(double x, int p) {
+    if(p <= 2) {
+      return Math.pow(x, p);
+    }
+    double tmp = x, ret = (p & 1) == 1 ? x : 1.;
+    p >>= 1;
+    while(true) {
+      if(p == 1) {
+        return ret * tmp;
+      }
+      if(p == 2) {
+        return ret * Math.pow(tmp, 2);
+      }
+      if((p & 1) != 0) {
         ret *= tmp;
       }
       tmp *= tmp;
@@ -157,7 +185,7 @@ public class PowiBenchmark {
   @Benchmark
   public double pow() {
     double ret = 0;
-    for (int j = 0; j < array.length; j++) {
+    for(int j = 0; j < array.length; j++) {
       ret += Math.pow(array[j], exponent);
     }
     return ret;
@@ -166,7 +194,7 @@ public class PowiBenchmark {
   @Benchmark
   public double powi() {
     double ret = 0;
-    for (int j = 0; j < array.length; j++) {
+    for(int j = 0; j < array.length; j++) {
       ret += powi(array[j], exponent);
     }
     return ret;
@@ -175,7 +203,7 @@ public class PowiBenchmark {
   @Benchmark
   public double powi2() {
     double ret = 0;
-    for (int j = 0; j < array.length; j++) {
+    for(int j = 0; j < array.length; j++) {
       ret += powi2(array[j], exponent);
     }
     return ret;
@@ -184,7 +212,7 @@ public class PowiBenchmark {
   @Benchmark
   public double powi3() {
     double ret = 0;
-    for (int j = 0; j < array.length; j++) {
+    for(int j = 0; j < array.length; j++) {
       ret += powi3(array[j], exponent);
     }
     return ret;
@@ -193,7 +221,7 @@ public class PowiBenchmark {
   @Benchmark
   public double powi4() {
     double ret = 0;
-    for (int j = 0; j < array.length; j++) {
+    for(int j = 0; j < array.length; j++) {
       ret += powi4(array[j], exponent);
     }
     return ret;
@@ -202,7 +230,16 @@ public class PowiBenchmark {
   @Benchmark
   public double powi5() {
     double ret = 0;
-    for (int j = 0; j < array.length; j++) {
+    for(int j = 0; j < array.length; j++) {
+      ret += powi5(array[j], exponent);
+    }
+    return ret;
+  }
+
+  @Benchmark
+  public double powi6() {
+    double ret = 0;
+    for(int j = 0; j < array.length; j++) {
       ret += powi5(array[j], exponent);
     }
     return ret;
